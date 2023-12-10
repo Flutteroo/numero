f70fc6c3f38bba229a981b6c5bd8c80742bc0e5e
Emanuele Tozzato <etozzato@gmail.com>
2023-12-10 00:49:06 -0800

    wrap up

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 
 import 'painters/grid_painter.dart';
@@ -25,6 +26,20 @@ enum Control {
   toggle_number,
   toggle_barcode,
   toggle_qrcode
+}
+
+class InputMap {
+  var isBarcode;
+  var isQRCode;
+  var isNumber;
+
+  InputMap({
+    this.isBarcode = false,
+    this.isQRCode = false,
+    this.isNumber = false,
+  });
+
+  get isAny => isBarcode || isQRCode || isNumber;
 }
 
 class Numero extends StatelessWidget {
@@ -55,26 +70,44 @@ class InitScreen extends StatefulWidget {
 }
 
 class _InitScreenState extends State<InitScreen> with WidgetsBindingObserver {
-  CameraDescription? camera;
   CameraController? controller;
+  CameraDescription? camera;
   CustomPaint gridPaint =
       CustomPaint(size: Size.infinite, painter: GridPainter());
 
-  String? reading; // = 31.0254;
-  String? id; // = 'AA219872EE';
-  bool isLoading = false;
-  bool isInitialized = false;
   bool isBusy = false;
-  BarcodeScanner barcodeScanner = GoogleMlKit.vision.barcodeScanner();
-  TextDetector textDetector = GoogleMlKit.vision.textDetector();
-  Map<String, String> instructions = {};
+  bool isInitialized = false;
+  bool isLoading = false;
   double zoomLevel = 0.0, minZoomLevel = 0.0, maxZoomLevel = 0.0;
+
+  String? id; // = 'AA219872EE';
+  String? reading; // = 31.0254;
+
+  TextDetector textDetector = GoogleMlKit.vision.textDetector();
+  BarcodeScanner barcodeScanner = GoogleMlKit.vision.barcodeScanner();
+
+  ValueNotifier<InputMap> inputMap = ValueNotifier(
+      InputMap(isBarcode: false, isQRCode: false, isNumber: false));
 
   @override
   void initState() {
     super.initState();
-    _ambiguate(WidgetsBinding.instance)?.addObserver(this);
+    _ambiguate(WidgetsBinding.instance).addObserver(this);
     _initializeCamera();
+    _shouldStream();
+  }
+
+  void _shouldStream() async {
+    if (inputMap.value.isAny) {
+      if (controller != null && controller!.value.isStreamingImages) {
+        await controller?.stopImageStream();
+      }
+
+      controller?.startImageStream(_processImage);
+    } else {
+      await controller?.stopImageStream();
+      return;
+    }
   }
 
   @override
@@ -128,11 +161,13 @@ class _InitScreenState extends State<InitScreen> with WidgetsBindingObserver {
                                           child: gridPaint,
                                         ),
                                       Positioned(
-                                        bottom: 100,
+                                        bottom: 50,
                                         left: 50,
                                         right: 50,
                                         child: Slider(
                                           value: zoomLevel,
+                                          label:
+                                              "${zoomLevel.toStringAsFixed(1)}x",
                                           min: minZoomLevel,
                                           max: maxZoomLevel,
                                           onChanged: (newSliderValue) {
@@ -142,10 +177,6 @@ class _InitScreenState extends State<InitScreen> with WidgetsBindingObserver {
                                                   .setZoomLevel(zoomLevel);
                                             });
                                           },
-                                          divisions:
-                                              (maxZoomLevel - 1).toInt() < 1
-                                                  ? null
-                                                  : (maxZoomLevel - 1).toInt(),
                                         ),
                                       )
                                     ],
@@ -156,36 +187,55 @@ class _InitScreenState extends State<InitScreen> with WidgetsBindingObserver {
                           ),
                         ),
                       ),
-                      Buttons(flowControl: flowControl)
+                      Buttons(inputMap: inputMap),
                     ],
                   )));
   }
 
   Widget _display() {
-    return Column(
-      children: <Widget>[
-        Text(
-          reading != null ? reading.toString() : 'no reading',
-          style: TextStyle(
-              fontSize: 60,
-              fontWeight: FontWeight.bold,
-              color: reading != null ? Colors.black54 : Colors.black26),
-        ),
-        Text(
-          id != null ? id.toString() : 'unidentified',
-          style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: id != null ? Colors.black45 : Colors.black12),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          InkWell(
+            onTap: () =>
+                Clipboard.setData(ClipboardData(text: reading.toString())),
+            onLongPress: () => setState(() {
+              reading = null;
+            }),
+            child: Text(
+              reading != null ? reading.toString() : 'no reading',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: reading != null ? Colors.black54 : Colors.black26),
+            ),
+          ),
+          InkWell(
+            onTap: () => Clipboard.setData(ClipboardData(text: id.toString())),
+            onLongPress: () => setState(() {
+              id = null;
+            }),
+            child: Text(
+              id != null ? id.toString() : 'unidentified',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: id != null ? Colors.black45 : Colors.black12),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   Future<void> _detectImage(InputImage inputImage) async {
     if (isBusy) return;
     isBusy = true;
-    if (instructions['category'] == 'number') {
+
+    if (inputMap.value.isNumber) {
       final recognisedText = await textDetector.processImage(inputImage);
       if (recognisedText.text != '') {
         setState(() {
@@ -193,8 +243,7 @@ class _InitScreenState extends State<InitScreen> with WidgetsBindingObserver {
         });
       }
     }
-    if (instructions['category'] == 'qrcode' ||
-        instructions['category'] == 'barcode') {
+    if (inputMap.value.isQRCode || inputMap.value.isBarcode) {
       final barcodes = await barcodeScanner.processImage(inputImage);
       if (barcodes.isNotEmpty) {
         setState(() {
@@ -243,49 +292,15 @@ class _InitScreenState extends State<InitScreen> with WidgetsBindingObserver {
     _detectImage(inputImage);
   }
 
-  void flowControl(Control control) async {
-    if (controller == null) return;
-
-    final _instructions = Map<String, String>.fromIterables(
-        ['type', 'operation', 'category'],
-        control.toString().split(RegExp(r'\.|_')));
-
-    setState(() {
-      instructions = _instructions;
-    });
-
-    if (instructions['operation'] == 'stop') {
-      await controller?.stopImageStream();
-      return;
-    }
-
-    if (instructions['operation'] == 'start') {
-      if (controller != null && controller!.value.isStreamingImages) {
-        await controller?.stopImageStream();
-      }
-
-      controller?.startImageStream(_processImage);
-    }
-
-    // start_number,
-    // start_barcode,
-    // start_qrcode,
-    // stop_number,
-    // stop_barcode,
-    // stop_qrcode,
-    // toggle_number,
-    // toggle_barcode,
-    // toggle_qrcode,
-  }
-
   Future<void> _initializeCamera() async {
     setState(() {
       isLoading = true;
     });
 
     List<CameraDescription> allCameras = await availableCameras();
-    camera = allCameras.firstWhere(((cameraDescription) =>
-        cameraDescription.lensDirection == CameraLensDirection.back));
+    // camera = allCameras.firstWhere(((cameraDescription) =>
+    //     cameraDescription.lensDirection == CameraLensDirection.back));
+    camera = allCameras.first;
     if (camera != null) _initializeControllerWithCamera(camera!);
 
     setState(() {
@@ -300,7 +315,7 @@ class _InitScreenState extends State<InitScreen> with WidgetsBindingObserver {
 
     final CameraController cameraController = CameraController(
       camera,
-      ResolutionPreset.veryHigh,
+      ResolutionPreset.low,
       enableAudio: false,
     );
 
@@ -323,7 +338,7 @@ class _InitScreenState extends State<InitScreen> with WidgetsBindingObserver {
         minZoomLevel = value;
       });
       cameraController.getMaxZoomLevel().then((value) {
-        maxZoomLevel = value;
+        maxZoomLevel = 16.0;
       });
       setState(() {});
     });
@@ -332,9 +347,9 @@ class _InitScreenState extends State<InitScreen> with WidgetsBindingObserver {
 }
 
 class Buttons extends StatefulWidget {
-  const Buttons({Key? key, required this.flowControl}) : super(key: key);
+  Buttons({Key? key, required this.inputMap}) : super(key: key);
 
-  final Function(Control) flowControl;
+  ValueNotifier<InputMap> inputMap;
 
   @override
   State<Buttons> createState() => _ButtonsState();
@@ -342,69 +357,47 @@ class Buttons extends StatefulWidget {
 
 class _ButtonsState extends State<Buttons> {
   bool isloading = false;
-  bool isNumber = false;
-  bool isBarcode = false;
-  bool isQRcode = false;
 
   void controlHandler(Control control) {
-    widget.flowControl(control);
     switch (control) {
       case Control.start_number:
-        setState(() {
-          isNumber = true;
-          isBarcode = false;
-          isQRcode = false;
-        });
+        widget.inputMap.value.isNumber = true;
+        widget.inputMap.value.isBarcode = false;
+        widget.inputMap.value.isQRCode = false;
         break;
       case Control.start_barcode:
-        setState(() {
-          isNumber = false;
-          isBarcode = true;
-          isQRcode = false;
-        });
+        widget.inputMap.value.isNumber = false;
+        widget.inputMap.value.isBarcode = true;
+        widget.inputMap.value.isQRCode = false;
         break;
       case Control.start_qrcode:
-        setState(() {
-          isNumber = false;
-          isBarcode = false;
-          isQRcode = true;
-        });
+        widget.inputMap.value.isNumber = false;
+        widget.inputMap.value.isBarcode = false;
+        widget.inputMap.value.isQRCode = true;
         break;
       case Control.stop_number:
-        setState(() {
-          isNumber = false;
-        });
+        widget.inputMap.value.isNumber = false;
         break;
       case Control.stop_barcode:
-        setState(() {
-          isBarcode = false;
-        });
+        widget.inputMap.value.isBarcode = false;
         break;
       case Control.stop_qrcode:
-        setState(() {
-          isQRcode = false;
-        });
+        widget.inputMap.value.isQRCode = false;
         break;
       case Control.toggle_number:
-        setState(() {
-          isQRcode = false;
-          isBarcode = false;
-          isNumber = !isNumber;
-        });
+        widget.inputMap.value.isNumber = !widget.inputMap.value.isNumber;
+        widget.inputMap.value.isBarcode = false;
+        widget.inputMap.value.isQRCode = false;
         break;
       case Control.toggle_barcode:
-        setState(() {
-          isNumber = false;
-          isQRcode = false;
-          isBarcode = !isBarcode;
-        });
+        widget.inputMap.value.isNumber = false;
+        widget.inputMap.value.isBarcode = !widget.inputMap.value.isBarcode;
+        widget.inputMap.value.isQRCode = false;
         break;
       case Control.toggle_qrcode:
-        setState(() {
-          isNumber = false;
-          isBarcode = false;
-          isQRcode = !isQRcode;
-        });
+        widget.inputMap.value.isNumber = false;
+        widget.inputMap.value.isBarcode = false;
+        widget.inputMap.value.isQRCode = !widget.inputMap.value.isQRCode;
         break;
     }
   }
@@ -416,7 +409,9 @@ class _ButtonsState extends State<Buttons> {
       children: [
         Ink(
           decoration: ShapeDecoration(
-            color: isNumber ? Colors.blue.shade400 : Colors.blue,
+            color: widget.inputMap.value.isNumber
+                ? Colors.blue.shade400
+                : Colors.blue,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
           ),
@@ -439,7 +434,9 @@ class _ButtonsState extends State<Buttons> {
         ),
         Ink(
           decoration: ShapeDecoration(
-            color: isBarcode ? Colors.blue.shade400 : Colors.blue,
+            color: widget.inputMap.value.isBarcode
+                ? Colors.blue.shade400
+                : Colors.blue,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
           ),
@@ -456,7 +453,9 @@ class _ButtonsState extends State<Buttons> {
         ),
         Ink(
           decoration: ShapeDecoration(
-            color: isQRcode ? Colors.blue.shade400 : Colors.blue,
+            color: widget.inputMap.value.isQRCode
+                ? Colors.blue.shade400
+                : Colors.blue,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
           ),
